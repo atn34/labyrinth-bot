07cd953b5c3cc534f044553b996cc2ae603f9c1d
Andrew Noyes <anoyes34@gmail.com>
2017-08-03 18:42:47 -0700

    Add some simple documentation

--- util.h
+++ util.h
@@ -3,9 +3,13 @@
 #include "highgui.h"
 
 struct DeleteImage {
-  int operator()(IplImage *img) { cvReleaseImage(&img); }
+  // Release the image.
+  void operator()(IplImage *img) { cvReleaseImage(&img); }
 };
 
+// Smart pointer for IplImage.
+//
+// Releases the image in destructor.
 using UniqueImage = std::unique_ptr<IplImage, DeleteImage>;
 
 inline UniqueImage LoadUniqueImage(const char *filename) {
