7dcde75bb7d4f5386d0705632d7cf2c3b6d370f5
Andrew Noyes <anoyes34@gmail.com>
2017-08-03 17:24:48 -0700

    Add travis

---
+++ install-deps.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -euxo pipefail
+
+
+# opencv
+
+wget https://github.com/opencv/opencv/archive/3.2.0.tar.gz -O opencv-3.2.0.tar.gz
+tar -xvf opencv-3.2.0.tar.gz
+pushd opencv-3.2.0
+mkdir build
+pushd build
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make -j8
+make install
+popd
