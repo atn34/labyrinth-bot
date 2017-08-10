7c2711fa3135e5947bf1d199941f008df7cec4ff
Andrew Noyes <anoyes34@gmail.com>
2017-08-10 11:26:32 -0700

    Do Canny edge detection

--- display.cc
+++ display.cc
@@ -4,9 +4,18 @@
 #include "util.h"
 
 int main(int argc, char **argv) {
-  auto img = LoadUniqueImage("labyrinth.png");
+  auto capture = UniqueCaptureFromCamera(0);
+  double fps = cvGetCaptureProperty(capture.get(), CV_CAP_PROP_FPS);
   cvNamedWindow("Example1", CV_WINDOW_AUTOSIZE);
-  cvShowImage("Example1", img.get());
-  cvWaitKey(0);
+  for (;;) {
+    IplImage *frame = cvQueryFrame(capture.get());
+    auto img = CreateUniqueImage(frame->width, frame->height, IPL_DEPTH_8U, 1);
+    cvCanny(frame, img.get(), 10, 100, 3);
+    cvShowImage("Example1", img.get());
+    int wait_ms = static_cast<int>(1000 / fps);
+    if (cvWaitKey(wait_ms) > 0) {
+      break;
+    }
+  }
   cvDestroyWindow("Example1");
 }
--- util.h
+++ util.h
@@ -13,6 +13,23 @@ struct DeleteImage {
 /// Releases the image in destructor.
 using UniqueImage = std::unique_ptr<IplImage, DeleteImage>;
 
-inline UniqueImage LoadUniqueImage(const char *filename) {
-  return UniqueImage(cvLoadImage(filename));
+inline UniqueImage CreateUniqueImage(int width, int height, int depth,
+                                     int nChannels) {
+  IplImage *img = cvCreateImage(cvSize(width, height), depth, nChannels);
+  return UniqueImage(img);
+}
+
+struct DeleteCapture {
+  /// Release the capture.
+  ///
+  void operator()(CvCapture *cap) { cvReleaseCapture(&cap); }
+};
+
+/// Smart pointer for CvCapture.
+///
+/// Releases the capture in destructor.
+using UniqueCapture = std::unique_ptr<CvCapture, DeleteCapture>;
+
+inline UniqueCapture UniqueCaptureFromCamera(int cam) {
+  return UniqueCapture(cvCreateCameraCapture(cam));
 }
