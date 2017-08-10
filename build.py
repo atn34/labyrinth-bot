1145a65830b67cfdd27cd1d6e9e17bd9e53205da
Andrew Noyes <anoyes34@gmail.com>
2017-08-10 10:07:29 -0700

    Stop calling pkg-config redundantly

--- build.py
+++ build.py
@@ -60,25 +60,28 @@ def main():
         else:
             assert False
 
+    cflags = subprocess.check_output(
+            'pkg-config --cflags opencv', shell=True).decode('utf-8')[:-1] + ' '.join(['-std=c++11'])
+    lflags = subprocess.check_output('pkg-config --libs opencv', shell=True).decode('utf-8')[:-1]
+
     for cc_file in itertools.chain(cc_files, test_cc_files):
         sys.stdout.write(subprocess.check_output(
-            'g++ -M -std=c++11 %s `pkg-config --cflags opencv`' % (
-                cc_file), shell=True).decode('utf-8'))
-        sys.stdout.write('\tg++ -std=c++11 -c %s `pkg-config --cflags opencv` -o $@\n' % (cc_file))
+            'g++ -M ' + cflags + ' ' + cc_file, shell=True).decode('utf-8'))
+        sys.stdout.write('\tg++ -std=c++11 -c %s %s -o $@\n' % (cc_file, cflags))
 
     object_files = [os.path.splitext(cc_file)[0]+'.o' for cc_file in cc_files]
 
     sys.stdout.write('a.out: ' + ' '.join(object_files) + '\n')
     sys.stdout.write('\tg++ -std=c++11 ' +
                      ' '.join(object_files) +
-                     ' `pkg-config --libs opencv` -o $@\n')
+                     ' %s -o $@\n' % lflags)
 
     for test_cc_file in test_cc_files:
         deps = list(object_deps(test_cc_file))
         sys.stdout.write(test_cc_file + '.exe: ' + ' '.join(deps) + '\n')
         sys.stdout.write('	g++ -std=c++11 ' +
                          ' '.join(deps) +
-                         ' `pkg-config --libs opencv` -lgtest -lpthread -lgtest_main -o $@\n')
+                         ' %s -lgtest -lpthread -lgtest_main -o $@\n' % lflags)
 
     sys.stdout.write('.PHONY: test\n')
     sys.stdout.write('test: ' + ' '.join(
