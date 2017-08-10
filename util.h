f3663d35a0f4a3b63b226232168e7bdfeafad6f7
Andrew Noyes <anoyes34@gmail.com>
2017-08-09 17:35:21 -0700

    Doxygen syntax for documentation comments

--- util.h
+++ util.h
@@ -3,13 +3,14 @@
 #include "highgui.h"
 
 struct DeleteImage {
-  // Release the image.
+  /// Release the image.
+  ///
   void operator()(IplImage *img) { cvReleaseImage(&img); }
 };
 
-// Smart pointer for IplImage.
-//
-// Releases the image in destructor.
+/// Smart pointer for IplImage.
+///
+/// Releases the image in destructor.
 using UniqueImage = std::unique_ptr<IplImage, DeleteImage>;
 
 inline UniqueImage LoadUniqueImage(const char *filename) {
