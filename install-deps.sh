59c77b3f6f2356c0f570b8fc7f2e6502d113c249
Andrew Noyes <anoyes34@gmail.com>
2017-08-10 10:57:51 -0700

    Update opencv

--- install-deps.sh
+++ install-deps.sh
@@ -4,22 +4,25 @@ set -euxo pipefail
 
 # opencv
 
+OPENCV_VERSION=3.3.0
+
 if ! pkg-config --exists opencv ; then 
-    wget https://github.com/opencv/opencv/archive/3.2.0.tar.gz -O opencv-3.2.0.tar.gz
-    tar -xvf opencv-3.2.0.tar.gz
-    pushd opencv-3.2.0
+    wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.tar.gz -O opencv-${OPENCV_VERSION}.tar.gz
+    tar -xvf opencv-${OPENCV_VERSION}.tar.gz
+    pushd opencv-${OPENCV_VERSION}
     mkdir build
     pushd build
     cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
     make -j8
-    make install
+    sudo make install
     popd
     popd
 fi
 
 # gtest
 
-cd /usr/src/gtest
+pushd /usr/src/gtest
 sudo cmake CMakeLists.txt
 sudo make
 sudo cp *.a /usr/lib
+popd
