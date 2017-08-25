fa1454a798d3222749dfdbdf0bf4395144104f0f
Andrew Noyes <anoyes34@gmail.com>
2017-08-25 14:37:56 -0700

    Estimate ball x, dx/dv, d^2x/dv^2 in pixels/frame

--- measure_ball_position_main.cc
+++ measure_ball_position_main.cc
@@ -16,6 +16,27 @@ int main(int, char **) {
   ThresholdBall threshold_ball;
 
   Mat src, transformed, masked, ball_coords;
+
+  // Based on http://opencvexamples.blogspot.com/2014/01/kalman-filter-implementation-tracking.html
+  KalmanFilter kf(/* dimension of state */ 6, /* dimension of measurement */ 2);
+  /* clang-format off */
+  kf.transitionMatrix = (Mat_<float>(6, 6)
+   << 1,0,1,0,.5, 0,
+      0,1,0,1, 0,.5,
+      0,0,1,0, 1, 0,
+      0,0,0,1, 0, 1,
+      0,0,0,0, 1, 0,
+      0,0,0,0, 0, 1);
+  /* clang-format on */
+
+  Mat_<float> measurement(2,1); measurement.setTo(Scalar(0));
+
+  setIdentity(kf.measurementMatrix);
+  setIdentity(kf.processNoiseCov, Scalar::all(1e-4));
+  setIdentity(kf.measurementNoiseCov, Scalar::all(1e-2));
+  setIdentity(kf.errorCovPost, Scalar::all(.1));
+
+  bool initialized_kf = false;
   for (;;) {
     cap >> src;
     if (!src.data) {
@@ -51,16 +72,33 @@ int main(int, char **) {
       ++info.size;
     });
 
+    if (infos.size() == 0) {
+      continue;
+    }
+
     std::sort(infos.begin(), infos.end(),
               [](const ComponentInfo &x, const ComponentInfo &y) {
                 return x.size > y.size;
               });
 
-    if (infos.size() > 0) {
-      const auto &ball_info = infos[0];
-      circle(transformed, Point(ball_info.xs / ball_info.size,
-                                ball_info.ys / ball_info.size),
-             10, Scalar(0, 0, 255));
+    const auto &ball_info = infos[0];
+    measurement(0) = ball_info.xs / ball_info.size;
+    measurement(1) = ball_info.ys / ball_info.size;
+
+    if (!initialized_kf) {
+      kf.statePre.at<float>(0) = measurement(0);
+      kf.statePre.at<float>(1) = measurement(1);
+      kf.statePre.at<float>(2) = 0;
+      kf.statePre.at<float>(3) = 0;
+      kf.statePre.at<float>(4) = 0;
+      kf.statePre.at<float>(5) = 0;
+      initialized_kf = true;
+    } else {
+      kf.predict();
+      Mat estimated = kf.correct(measurement);
+      Point statePt(estimated.at<float>(0), estimated.at<float>(1));
+
+      circle(transformed, statePt, 10, Scalar(0, 0, 255));
     }
 
     imshow("Ball position", transformed);
