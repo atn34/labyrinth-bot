c947ffcdce7555334df373ad48e95f9dc7b67e74
Andrew Noyes <anoyes34@gmail.com>
2017-08-24 17:29:13 -0700

    const Mat& ball mask

--- perspective_transform_main.cc
+++ perspective_transform_main.cc
@@ -7,19 +7,22 @@
 
 using namespace cv;
 
-Mat get_ball_mask() {
-  Mat board = imread("board.png");
-  Mat out(board.rows, board.cols, CV_8U);
-  for (int r = 0; r < board.rows; ++r) {
-    for (int c = 0; c < board.cols; ++c) {
-      const auto& point = board.at<Vec3b>(r, c);
-      if (point[0] == 220 && point[1] == 0 && point[2] == 255) {
-        out.at<uchar>(r, c) = 0;
-      } else {
-        out.at<uchar>(r, c) = 1;
+const Mat &get_ball_mask() {
+  static const Mat &out = []() {
+    Mat board = imread("board.png");
+    auto *result = new Mat(board.rows, board.cols, CV_8U);
+    for (int r = 0; r < board.rows; ++r) {
+      for (int c = 0; c < board.cols; ++c) {
+        const auto &point = board.at<Vec3b>(r, c);
+        if (point[0] == 220 && point[1] == 0 && point[2] == 255) {
+          result->at<uchar>(r, c) = 0;
+        } else {
+          result->at<uchar>(r, c) = 1;
+        }
       }
     }
-  }
+    return *result;
+  }();
   return out;
 }
 
