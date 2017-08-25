12227abaa7db92983cbcd3539597695f92f3df38
Andrew Noyes <anoyes34@gmail.com>
2017-08-25 16:48:31 -0700

    Set FPS to 60, delete initialized_kf
    Observing that the estimate took a second to converge on the ball
    anyway, it seems that initialized_kf logic wasn't working anyway.

--- measure_ball_position_main.cc
+++ measure_ball_position_main.cc
@@ -8,6 +8,7 @@ using namespace cv;
 
 int main(int, char **) {
   VideoCapture cap(1);
+  cap.set(CV_CAP_PROP_FPS, 60);
   if (!cap.isOpened())
     return -1;
   namedWindow("Ball position", CV_WINDOW_AUTOSIZE);
@@ -30,10 +31,18 @@ int main(int, char **) {
 
   setIdentity(kf.measurementMatrix);
   setIdentity(kf.processNoiseCov, Scalar::all(1e-4));
-  setIdentity(kf.measurementNoiseCov, Scalar::all(1e-1));
-  setIdentity(kf.errorCovPost, Scalar::all(.1));
+  setIdentity(kf.measurementNoiseCov, Scalar::all(1e1));
+  kf.measurementNoiseCov.at<float>(0, 0) = 1;
+  kf.measurementNoiseCov.at<float>(1, 1) = 1;
+  setIdentity(kf.errorCovPost, Scalar::all(1e-1));
 
-  bool initialized_kf = false;
+  kf.statePre.at<float>(0) = measurement(0);
+  kf.statePre.at<float>(1) = measurement(1);
+  kf.statePre.at<float>(2) = 0;
+  kf.statePre.at<float>(3) = 0;
+  kf.statePre.at<float>(4) = 0;
+  kf.statePre.at<float>(5) = 0;
+
   for (;;) {
     cap >> src;
     if (!src.data) {
@@ -54,35 +63,25 @@ int main(int, char **) {
     measurement(0) = measured.x;
     measurement(1) = measured.y;
 
-    if (!initialized_kf) {
-      kf.statePre.at<float>(0) = measurement(0);
-      kf.statePre.at<float>(1) = measurement(1);
-      kf.statePre.at<float>(2) = 0;
-      kf.statePre.at<float>(3) = 0;
-      kf.statePre.at<float>(4) = 0;
-      kf.statePre.at<float>(5) = 0;
-      initialized_kf = true;
-    } else {
-      kf.predict();
-      Mat estimated = kf.correct(measurement);
-      Point statePt(estimated.at<float>(0), estimated.at<float>(1));
+    kf.predict();
+    Mat estimated = kf.correct(measurement);
+    Point statePt(estimated.at<float>(0), estimated.at<float>(1));
 
-      float t_delta = 75 / 16; // approximate cam latency in frames
-      float x = estimated.at<float>(0);
-      float y = estimated.at<float>(1);
-      float vx = estimated.at<float>(2);
-      float vy = estimated.at<float>(3);
-      float ax = estimated.at<float>(4);
-      float ay = estimated.at<float>(5);
-      Point currentPt(x + t_delta * vx + 0.5 * t_delta * t_delta * ax,
-               y + t_delta * vy + 0.5 * t_delta * t_delta * ay);
+    float t_delta = 75 / 16; // approximate cam latency in frames
+    float x = estimated.at<float>(0);
+    float y = estimated.at<float>(1);
+    float vx = estimated.at<float>(2);
+    float vy = estimated.at<float>(3);
+    float ax = estimated.at<float>(4);
+    float ay = estimated.at<float>(5);
+    Point currentPt(x + t_delta * vx + 0.5 * t_delta * t_delta * ax,
+              y + t_delta * vy + 0.5 * t_delta * t_delta * ay);
 
-      circle(transformed, currentPt, 10, Scalar(0, 0, 255));
-    }
+    circle(transformed, currentPt, 10, Scalar(0, 0, 255));
 
     imshow("Ball position", transformed);
 
-    if (cvWaitKey(30) > 0) {
+    if (cvWaitKey(1) > 0) {
       break;
     }
   }
