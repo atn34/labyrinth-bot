cdee42a96f4af32de8aaea029ce27ed834abfcc7
Andrew Noyes <anoyes34@gmail.com>
2017-08-27 22:08:36 -0700

    Fix CI

--- install-deps.sh
+++ install-deps.sh
@@ -5,19 +5,20 @@ set -euxo pipefail
 # opencv
 
 OPENCV_VERSION=3.3.0
+OPENCV_TARBALL=opencv-${OPENCV_VERSION}.tar.gz
 
-if ! pkg-config --exists opencv ; then 
-    wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.tar.gz -O opencv-${OPENCV_VERSION}.tar.gz
-    tar -xvf opencv-${OPENCV_VERSION}.tar.gz
-    pushd opencv-${OPENCV_VERSION}
-    mkdir build
-    pushd build
-    cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
-    make -j8
-    sudo make install
-    popd
-    popd
-fi
+wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.tar.gz -O opencv-${OPENCV_VERSION}.tar.gz
+tar -xvf ${OPENCV_TARBALL}
+rm ${OPENCV_TARBALL}
+pushd opencv-${OPENCV_VERSION}
+mkdir build
+pushd build
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make -j8
+sudo make install
+popd
+popd
+rm -rf opencv-${OPENCV_VERSION}
 
 # googletest
 
@@ -25,6 +26,7 @@ GOOGLETEST_VERSION=1.8.0
 GOOGLETEST_TARBALL=googletest-${GOOGLETEST_VERSION}.tar.gz
 wget https://github.com/google/googletest/archive/release-${GOOGLETEST_VERSION}.tar.gz -O ${GOOGLETEST_TARBALL}
 tar -xvf ${GOOGLETEST_TARBALL}
+rm ${GOOGLETEST_TARBALL}
 pushd googletest-release-${GOOGLETEST_VERSION}
 mkdir -p build
 pushd build
@@ -33,22 +35,23 @@ make -j8
 sudo make install
 popd
 popd
+rm -rf googletest-release-${GOOGLETEST_VERSION}
 
 # libsocket
 
 LIBSOCKET_VERSION=2.4.1
 LIBSOCKET_TARNAME=libsocket-${LIBSOCKET_VERSION}.tar.gz
 
-if ! [ -f /usr/lib/libsocket++.so ] ; then 
-    wget https://github.com/dermesser/libsocket/archive/v${LIBSOCKET_VERSION}.tar.gz -O ${LIBSOCKET_TARNAME}
-    tar -xvf ${LIBSOCKET_TARNAME}
-    pushd libsocket-${LIBSOCKET_VERSION}
-    mkdir -p build
-    pushd build
-    cmake ..
-    make -j8
-    sudo make install
-    popd
-    popd
-fi
+wget https://github.com/dermesser/libsocket/archive/v${LIBSOCKET_VERSION}.tar.gz -O ${LIBSOCKET_TARNAME}
+tar -xvf ${LIBSOCKET_TARNAME}
+rm ${LIBSOCKET_TARNAME}
+pushd libsocket-${LIBSOCKET_VERSION}
+mkdir -p build
+pushd build
+cmake ..
+make -j8
+sudo make install
+popd
+popd
+rm libsocket-${LIBSOCKET_VERSION}
 
