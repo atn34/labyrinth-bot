e366c12d6436d343ec3c7024f49f858582caedb1
Andrew Noyes <anoyes34@gmail.com>
2017-08-29 16:19:05 -0700

    WIP. It does.. something

--- measure_ball_position_main.cc
+++ measure_ball_position_main.cc
@@ -1,9 +1,11 @@
+#include <iostream>
+
 #include "opencv2/highgui.hpp"
 #include "opencv2/opencv.hpp"
 
 #include "measure_ball_position.h"
+#include "motor_client.h"
 #include "perspective_transform.h"
-#include "ring_buffer.h"
 
 using namespace cv;
 
@@ -29,6 +31,7 @@ int main(int, char **) {
   /* clang-format on */
 
   Mat_<float> measurement(2,1); measurement.setTo(Scalar(0));
+  Mat_<float> control(2,1); measurement.setTo(Scalar(0));
 
   setIdentity(kf.measurementMatrix);
   setIdentity(kf.processNoiseCov, Scalar::all(1e-4));
@@ -37,17 +40,16 @@ int main(int, char **) {
   kf.measurementNoiseCov.at<float>(1, 1) = 4;
   setIdentity(kf.errorCovPost, Scalar::all(1e-1));
 
-  kf.statePre.at<float>(0) = measurement(0);
-  kf.statePre.at<float>(1) = measurement(1);
-  kf.statePre.at<float>(2) = 0;
-  kf.statePre.at<float>(3) = 0;
-  kf.statePre.at<float>(4) = 0;
-  kf.statePre.at<float>(5) = 0;
+  MotorClient motor_client;
 
-  constexpr int cam_latency_in_frames = 4;
-  RingBuffer<Mat, cam_latency_in_frames + 1> estimate_buffer;
+  float x_goal = 187;
+  float y_goal = 15;
 
-  for (;;) {
+  float pending_x_steps = 0.0;
+  float pending_y_steps = 0.0;
+
+  int pause_motors_until_iter = 100;
+  for (int iter = 0;; ++iter) {
     cap >> src;
     if (!src.data) {
       return -1;
@@ -56,41 +58,67 @@ int main(int, char **) {
     flip(src, src, -1);
 
     if (!PerspectiveTransform(src, &transformed)) {
-      continue;
+      std::cout << "perspective transform failed" << std::endl;
+      break;
     }
 
     Point measured;
     if (!MeasureBallPosition(transformed, &measured)) {
-      continue;
+      std::cout << "finding ball failed" << std::endl;
+      break;
     }
 
     measurement(0) = measured.x;
     measurement(1) = measured.y;
 
     kf.predict();
-    Mat estimated = kf.correct(measurement).clone();
-    estimate_buffer.add(estimated);
+    Mat estimated = kf.correct(measurement);
 
-    if (estimate_buffer.size() == cam_latency_in_frames + 1) {
-      float t_delta = cam_latency_in_frames;
-      const Mat& old_estimate = estimate_buffer.get(0);
-      float x = old_estimate.at<float>(0);
-      float y = old_estimate.at<float>(1);
-      float vx = old_estimate.at<float>(2);
-      float vy = old_estimate.at<float>(3);
-      float ax = old_estimate.at<float>(4);
-      float ay = old_estimate.at<float>(5);
-      Point currentPointEstimate(x + t_delta * vx + 0.5 * t_delta * t_delta * ax,
-                      y + t_delta * vy + 0.5 * t_delta * t_delta * ay);
-
-      circle(transformed, currentPointEstimate, 10, Scalar(0, 0, 255));
-    }
-
-    imshow("Ball position", transformed);
-
-    if (cvWaitKey(1) > 0) {
+    int key = cvWaitKey(1);
+    if (key == 'q') {
       break;
     }
+
+    float x = estimated.at<float>(0);
+    float y = estimated.at<float>(1);
+    float vx = estimated.at<float>(2);
+    float vy = estimated.at<float>(3);
+    float ax = estimated.at<float>(4);
+    float ay = estimated.at<float>(5);
+
+    bool ball_at_rest = vx * vx + vy * vy < 0.1 && ax * ax + ay * ay < 0.001;
+
+    float p = 0.01;
+    float d = 0;
+    float dd = 0;
+
+    if (iter >= pause_motors_until_iter) {
+      pending_x_steps -= (x - x_goal) * p + vx * d + ax * dd;
+      pending_y_steps -= (y - y_goal) * p + vy * d + ay * dd;
+
+      control(0) = 0;
+      control(1) = 0;
+      if (floor(std::abs(pending_x_steps)) > 0) {
+        control(0) = floor(pending_x_steps);
+        pending_x_steps -= floor(pending_x_steps);
+      }
+
+      if (floor(std::abs(pending_y_steps)) > 0) {
+        control(1) = floor(pending_y_steps);
+        pending_y_steps -= floor(pending_y_steps);
+      }
+
+      pause_motors_until_iter = std::max(std::abs(control(0)), std::abs(control(1)));
+
+      motor_client.step(MotorClient::HORIZONTAL, control(0));
+      motor_client.step(MotorClient::VERTICAL, control(1));
+    }
+
+    circle(transformed, Point(x, y), 10,
+           ball_at_rest ? Scalar(255, 0, 0) : Scalar(0, 0, 255));
+    circle(transformed, Point(x_goal, y_goal), 10, Scalar(0, 255, 0));
+
+    imshow("Ball position", transformed);
   }
   return 0;
 }
