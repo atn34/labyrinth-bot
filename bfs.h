5239cbb4f2d0f76cdd5459bc1430e39996bfce0e
Andrew Noyes <anoyes34@gmail.com>
2017-09-01 10:30:22 -0700

    Switch to bfs, pull out into its own class

---
+++ bfs.h
@@ -0,0 +1,55 @@
+#ifndef BFS_H
+#define BFS_H
+
+#include <deque>
+#include <unordered_set>
+
+#include "opencv2/imgproc.hpp"
+
+class Bfs {
+public:
+  Bfs(const cv::Mat *img, std::unordered_set<int> *marked_for_visiting,
+      std::deque<int> *to_visit)
+      : img_(img), num_rows_(img->rows), num_cols_(img->cols),
+        marked_for_visiting_(marked_for_visiting), to_visit_(to_visit) {}
+  virtual ~Bfs() = default;
+
+  template <typename Lambda> void bfs(int node, Lambda f) {
+    to_visit_->clear();
+    to_visit_->push_back(node);
+    marked_for_visiting_->insert(node);
+    while (to_visit_->size() > 0) {
+      int current = to_visit_->front();
+      to_visit_->pop_front();
+      int row = current / num_cols_;
+      int col = current % num_cols_;
+      f(row, col);
+      for (int r = row - 1; r <= row + 1; ++r) {
+        for (int c = col - 1; c <= col + 1; ++c) {
+          if (img_->at<uchar>(row, col) == 0) {
+            continue;
+          }
+          if (!(c >= 0 && c < num_cols_ && r >= 0 && r < num_rows_)) {
+            continue;
+          }
+          int neighbor = r * num_cols_ + c;
+          if (marked_for_visiting_->find(neighbor) !=
+              marked_for_visiting_->end()) {
+            continue;
+          }
+          to_visit_->push_back(neighbor);
+          marked_for_visiting_->insert(neighbor);
+        }
+      }
+    }
+  }
+
+private:
+  const cv::Mat *img_;
+  int num_rows_;
+  int num_cols_;
+  std::unordered_set<int> *marked_for_visiting_;
+  std::deque<int> *to_visit_;
+};
+
+#endif /* BFS_H */
--- connected_components.h
+++ connected_components.h
@@ -1,10 +1,13 @@
 #ifndef CONNECTED_COMPONENTS_H
 #define CONNECTED_COMPONENTS_H
 
-#include <unordered_set>
 #include <assert.h>
+#include <deque>
+#include <unordered_set>
 
 #include "opencv2/imgproc.hpp"
+
+#include "bfs.h"
 
 class ConnectedComponentsVisitor {
 public:
@@ -25,49 +28,20 @@ public:
         if (marked_for_visiting_.find(node) != marked_for_visiting_.end()) {
           continue;
         }
-        DFS(node, f);
+        Bfs bfs(img_, &marked_for_visiting_, &to_visit_);
+        bfs.bfs(node, [&](int row, int col) { f(row, col, component_label_); });
         ++component_label_;
       }
     }
   }
 
 private:
-  template <typename Lambda> void DFS(int node, Lambda f) {
-    to_visit_.clear();
-    to_visit_.push_back(node);
-    marked_for_visiting_.insert(node);
-    while (to_visit_.size() > 0) {
-      int current = to_visit_.back();
-      to_visit_.pop_back();
-      int row = current / num_cols_;
-      int col = current % num_cols_;
-      f(row, col, component_label_);
-      for (int r = row - 1; r <= row + 1; ++r) {
-        for (int c = col - 1; c <= col + 1; ++c) {
-          if (img_->at<uchar>(row, col) == 0) {
-            continue;
-          }
-          if (!(c >= 0 && c < num_cols_ && r >= 0 && r < num_rows_)) {
-            continue;
-          }
-          int neighbor = r * num_cols_ + c;
-          if (marked_for_visiting_.find(neighbor) !=
-              marked_for_visiting_.end()) {
-            continue;
-          }
-          to_visit_.push_back(neighbor);
-          marked_for_visiting_.insert(neighbor);
-        }
-      }
-    }
-  }
-
   const cv::Mat *img_;
   int num_rows_;
   int num_cols_;
-  std::unordered_set<int> marked_for_visiting_;
   int component_label_ = 0;
-  std::vector<int> to_visit_;
+  std::unordered_set<int> marked_for_visiting_;
+  std::deque<int> to_visit_;
 };
 
-#endif  // CONNECTED_COMPONENTS_H
+#endif // CONNECTED_COMPONENTS_H
