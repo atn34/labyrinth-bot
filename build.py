038c4ac58375a6e8d7a4149a200403d1b07a9bff
Andrew Noyes <anoyes34@gmail.com>
2017-09-02 15:28:43 -0700

    Support .cpp, more robust comment stripping

--- build.py
+++ build.py
@@ -15,6 +15,9 @@ OPENCV_LIBS = set('''-lopencv_dnn -lopencv_ml -lopencv_objdetect -lopencv_shape
 -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs
 -lopencv_video -lopencv_photo -lopencv_imgproc -lopencv_flann -lopencv_core'''.split())
 
+def is_cc(file_name):
+    return file_name.endswith('.cc') or file_name.endswith('.cpp')
+
 class memoize:
     def __init__(self, fn):
         self.fn = fn
@@ -29,15 +32,15 @@ def includes_from_file(file_name):
     result = []
     if not os.path.isfile(file_name):
         return result
-    with open(file_name, 'r') as fil:
-        for line in fil:
-            if line.startswith('#') or line.startswith('//') or line == '\n':
-                match = INCLUDE_RE.match(line)
-                if match:
-                    include = match.groups()[0]
-                    result.append(include)
-            else:
-                break
+    for line in subprocess.check_output(
+            ['cpp', '-fpreprocessed', file_name]).decode('utf-8').splitlines():
+        if line.startswith('#') or line == '':
+            match = INCLUDE_RE.match(line)
+            if match:
+                include = match.groups()[0]
+                result.append(include)
+        else:
+            break
     return result
 
 class TransitiveIncludes(object):
@@ -59,7 +62,7 @@ class TransitiveIncludes(object):
         yield from self._read_includes(self.cc_file)
 
     def _read_includes(self, file_name):
-        if self.includes_from_cc and file_name.endswith('.cc'):
+        if self.includes_from_cc and is_cc(file_name):
             if file_name not in self.files_processed:
                 self.files_processed.add(file_name)
             else:
@@ -73,6 +76,8 @@ class TransitiveIncludes(object):
                     basename = os.path.splitext(include)[0]
                     if os.path.isfile(basename + '.cc'):
                         yield from self._read_includes(basename + '.cc')
+                    elif os.path.isfile(basename + '.cpp'):
+                        yield from self._read_includes(basename + '.cpp')
 
 
 def object_deps(file_name):
@@ -80,14 +85,16 @@ def object_deps(file_name):
     Reports all the object deps needed to compile the
     executable corresponding to the given cc file.
     """
-    assert file_name.endswith('.cc')
+    assert is_cc(file_name)
     for include in TransitiveIncludes(file_name, includes_from_cc=True).find():
         basename = os.path.splitext(include)[0]
         if os.path.isfile(basename + '.cc'):
             yield basename + '.o'
+        elif os.path.isfile(basename + '.cpp'):
+            yield basename + '.o'
 
 def header_deps(file_name, includes_from_cc=False):
-    assert file_name.endswith('.cc')
+    assert is_cc(file_name)
     yield from TransitiveIncludes(file_name, includes_from_cc=includes_from_cc).find()
 
 def headers_to_cflags(headers):
@@ -99,7 +106,7 @@ def headers_to_cflags(headers):
     cflags = set()
     for h_dep in headers:
         cflags |= header_to_cflags(h_dep)
-    return ' '.join(sorted(cflags) + ['-std=c++11', '-Wall', '-Werror', '-g'])
+    return ' '.join(sorted(cflags) + ['-I.', '-std=c++11', '-Wall', '-Werror', '-g'])
 
 def headers_to_lflags(headers):
     @memoize
@@ -116,7 +123,7 @@ def headers_to_lflags(headers):
     return ' '.join(sorted(lflags))
 
 def object_file_rule(cc_file):
-    assert cc_file.endswith('.cc')
+    assert is_cc(cc_file)
     h_deps = list(header_deps(cc_file))
     cflags = headers_to_cflags(h_deps)
     depends = os.path.splitext(cc_file)[0]+'.o: ' + cc_file + ' ' + ' '.join(filter(os.path.isfile, h_deps))
@@ -148,8 +155,8 @@ def main():
     test_cc_files = set()
     main_cc_files = set()
     for f in subprocess.check_output(
-            r'find . -type f -name "*.cc" | sort', shell=True).decode('utf-8').splitlines():
-        if f.endswith('.cc'):
+            r'find . -type f -name "*.c*" | sort', shell=True).decode('utf-8').splitlines():
+        if is_cc(f):
             cc_files.add(f)
         if f.endswith('_test.cc'):
             test_cc_files.add(f)
