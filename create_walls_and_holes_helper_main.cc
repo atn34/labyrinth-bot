017fc6e85e8d4cd6c0cb06b686063a015b18d4e1
Andrew Noyes <anoyes34@gmail.com>
2017-09-03 11:59:58 -0700

    Switch to axis aligned rectangles for walls

--- create_walls_and_holes_helper_main.cc
+++ create_walls_and_holes_helper_main.cc
@@ -7,27 +7,45 @@
 
 using namespace cv;
 
+struct MouseState {
+  Point mouse;
+  Point upper_left;
+  bool selecting_rectangle = false;
+};
+
 int main(int argc, char *argv[]) {
   Mat board = imread("board.png");
-  Point mouse;
 
   cvNamedWindow("create walls", CV_WINDOW_AUTOSIZE);
+
+  MouseState mouse_state;
 
   setMouseCallback(
       "create walls",
       +[](int event, int x, int y, int /* flags */, void *user_data) {
-        Point &mouse = *(Point *)user_data;
-        mouse.x = x;
-        mouse.y = y;
-
+        MouseState &mouse_state = *(MouseState *)user_data;
+        mouse_state.mouse.x = x;
+        mouse_state.mouse.y = y;
         if (event == EVENT_LBUTTONDOWN) {
-          std::cout << "{" << x << ", " << y << "}," << std::endl;
+          if (mouse_state.selecting_rectangle) {
+            mouse_state.selecting_rectangle = false;
+            std::cout << "{";
+            for (const Point &point :
+                 {mouse_state.upper_left, Point(x, mouse_state.upper_left.y),
+                  mouse_state.mouse, Point(mouse_state.upper_left.x, y)}) {
+              std::cout << "{" << point.x << "," << point.y << "},";
+            }
+            std::cout << "}," << std::endl;
+          } else {
+            mouse_state.selecting_rectangle = true;
+            mouse_state.upper_left = Point(x, y);
+          }
         }
         if (event == EVENT_RBUTTONDOWN) {
-          std::cout << "},{" << std::endl;
+          std::cout << "{" << x << "," << y << "}," << std::endl;
         }
       },
-      &mouse);
+      &mouse_state);
 
   for (const auto &polygon : WallPolygons()) {
     const MyPoint *last = nullptr;
@@ -49,11 +67,15 @@ int main(int argc, char *argv[]) {
     circle(board, Point(hole.x, hole.y), 16, Scalar(0, 255, 0));
   }
 
-  Mat with_cursor_circle;
+  Mat with_cursors;
   for (;;) {
-    with_cursor_circle = board.clone();
-    circle(with_cursor_circle, mouse, 16, Scalar(0, 0, 0));
-    imshow("create walls", with_cursor_circle);
+    with_cursors = board.clone();
+    if (mouse_state.selecting_rectangle) {
+      rectangle(with_cursors, mouse_state.upper_left, mouse_state.mouse, Scalar(0, 0, 0));
+    } else {
+      circle(with_cursors, mouse_state.mouse, 16, Scalar(0, 0, 0));
+    }
+    imshow("create walls", with_cursors);
 
     if (cvWaitKey(30) > 0) {
       break;
