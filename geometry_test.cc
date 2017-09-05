16a94731d57a6c6d0a88050ce2e07a798f81a934
Andrew Noyes <anoyes34@gmail.com>
2017-09-04 17:25:04 -0700

    Add some NoImpact tests

--- geometry_test.cc
+++ geometry_test.cc
@@ -16,6 +16,13 @@ TEST(DistanceToImpact, CircleCircleY) {
     EXPECT_EQ(5, DistanceToImpact(start, dir, target));
 }
 
+TEST(DistanceToImpact, CircleCircleNoImpact) {
+    Circle start{{0, 2}, 2};
+    Vec dir{1, 1};
+    Circle target{{0, 10}, 1};
+    EXPECT_LT(DistanceToImpact(start, dir, target), 0);
+}
+
 TEST(DistanceToImpact, CirclePerpendicularLineSegmentX) {
     Circle start{{2, 0}, 2};
     Vec dir{1, 0};
@@ -42,5 +49,12 @@ TEST(DistanceToImpact, CircleParallelLineSegmentFlipped) {
     Vec dir{1, 0};
     LineSegment target{{11, 0}, {9, 0}};
     EXPECT_EQ(5, DistanceToImpact(start, dir, target));
+}
+
+TEST(DistanceToImpact, CircleLineSegmentNoImpact) {
+    Circle start{{2, 0}, 2};
+    Vec dir{1, 1};
+    LineSegment target{{10, -1}, {10, 1}};
+    EXPECT_LT(DistanceToImpact(start, dir, target), 0);
 }
 
